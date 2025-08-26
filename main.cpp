012429d8df181617c99b39c6adf5f8ea77c64f2a
kacpereqo <remzak.k@gmail.com>
2025-08-26 12:12:56 +0200

    Sex

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//
+// Created by remza on 26.08.2025.
+//
+
+
+enum class TokenType{
+
+    /// Literals
+    IntLiteral,
+
+    /// Operators
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+};
+struct LexicalToken{
+
+
+};
+
+int main(){
+    return 0;
+}
